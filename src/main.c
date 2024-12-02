7c59fd8d6a0179dbd7fb46ca9a5685e06854c0a4
Suhayb <98946068+Poimurgh@users.noreply.github.com>
2024-12-02 01:19:07 +0100

    fonction compute_md5 + test sur le main
    du coup j'ai fait la fonction compute md5
    Aussi j'ai lu vite fait la partie d'alexis et du coup :
    Pr la Fonction list_files : j'y vois pas d'erreurs par contre en vrai pour l'allocation de fullpath il faudrait peutetre faire une allocation dynamique et utiliser la taille de path et d_name comme ca : ```char *fullpath = malloc(strlen(path) + strlen(entry->d_name) + 2);```  et aussi peutetre retourner une liste des fichiers
    
    pour read_files, il fauderait plutot retourner un string au lieu de juste print
    
    pour la 3eme func j'y vois rien a redire

--- src/main.c
+++ src/main.c
@@ -7,7 +7,30 @@
 #include "backup_manager.h"
 #include "network.h"
 
+void testMD5Compute(){
+    // Exemple de données pour lesquelles on va calculer le MD5
+    const char *data = "Hello, world!";
+    size_t len = strlen(data);  // Calculer la longueur des données
+    
+    // Tableau pour stocker le résultat du hash MD5 (16 octets)
+    unsigned char md5_hash[MD5_DIGEST_LENGTH];
+
+    // Calculer le MD5 des données
+    compute_md5((void *)data, len, md5_hash);
+
+    // Afficher le hash MD5 au format hexadécimal
+    printf("Hash MD5 : ");
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        printf("%02x", md5_hash[i]);  // Afficher chaque octet en hexadécimal
+    }
+    printf("\n");
+}
+
+
 int main(int argc, char *argv[]) {
+    // tests :
+    testMD5Compute();
+
     // Analyse des arguments de la ligne de commande
 
     // Implémentation de la logique de sauvegarde et restauration
