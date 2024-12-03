7abe8e7ebe27c929f33a236ada5b15dd4af51c07
Suhayb <98946068+Poimurgh@users.noreply.github.com>
2024-12-03 11:01:53 +0100

    aller, au travail le nain

--- src/file_handler.c
+++ src/file_handler.c
@@ -1,65 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
 #include <string.h>
+#include <fcntl.h>
+#include <dirent.h>
 #include "file_handler.h"
+#include "deduplication.h"
 
-void list_files(const char *path) {
- struct dirent *entry;
-    DIR *dp = opendir(path);
-    if (dp == NULL) {
-        perror("Erreur lors de l'ouverture du répertoire");
-        return;
-    }
-    while ((entry = readdir(dp)) != NULL) {
-        // Ignorer les entrées "." et ".."
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-            continue;
-        }
-        // Construire le chemin complet de l'entrée
-        char fullpath[1024];
-        snprintf(fullpath, sizeof(fullpath), "%s/%s", path, entry->d_name);
-        // Afficher l'entrée avec une indentation basée sur la profondeur
-        printf("%s\n",fullpath);
-
-        // Si l'entrée est un répertoire, explorer récursivement
-        if (entry->d_type == DT_DIR) {
-            // list_directory_recursive(fullpath);
-        }
-    }
+// Fonction permettant de lire un élément du fichier .backup_log
+log_t read_backup_log(const char *logfile){
+    /* Implémenter la logique pour la lecture d'une ligne du fichier ".backup_log"
+    * @param: logfile - le chemin vers le fichier .backup_log
+    * @return: une structure log_t
+    */
 }
 
+// Fonction permettant de mettre à jour une ligne du fichier .backup_log
+void update_backup_log(const char *logfile, log_t *logs){
+  /* Implémenter la logique de modification d'une ligne du fichier ".bakcup_log"
+  * @param: logfile - le chemin vers le fichier .backup_log
+  *         logs - qui est la liste de toutes les lignes du fichier .backup_log sauvegardée dans une structure log_t
+  */
 
-
-char* read_file(const char *filepath, size_t *size) {
-  FILE *file = fopen(filepath, "r"); // Ouvrir le fichier en mode lecture ("r")
-    if (file == NULL) {
-        perror("Erreur lors de l'ouverture du fichier");
-        return NULL;
-    }
-    char buffer[1024]; // Tampon pour stocker chaque ligne lue
-    printf("Contenu du fichier %s :\n", filepath);
-    // Lire ligne par ligne
-    while (fgets(buffer, sizeof(buffer), file)) {
-        printf("%s", buffer);
-    }
-
-    // Fermer le fichier
-    fclose(file);
 }
 
-void write_file(const char *filepath, const void *data, size_t size) {
-    FILE *file = fopen(filepath, "a"); // Ouvrir le fichier en mode ajout ("a")
-    if (file == NULL) {
-        perror("Erreur lors de l'ouverture du fichier");
-        return;
-    }
-        if (fprintf(file, "%s\n", data) < 0) { // Écrire et vérifier si erreurs
-            perror("Erreur lors de l'écriture dans le fichier");
-            fclose(file);
-            return;
-        }
-    printf("Données écrites avec succès dans le fichier %s\n", filepath);
-    // Fermer le fichier
-    fclose(file);
+void write_log_element(log_element *elt, FILE *logfile){
+  /* Implémenter la logique pour écrire un élément log de la liste chaînée log_element dans le fichier .backup_log
+   * @param: elt - un élément log à écrire sur une ligne
+   *         logfile - le chemin du fichier .backup_log
+   */
+}
+
+void list_files(const char *path){
+  /* Implémenter la logique pour lister les fichiers présents dans un répertoire
+  */
 }
